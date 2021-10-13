530450870948330c2d1d8df965d66a6c999cd58f
EdJeeOnGitHub <edjee96@gmail.com>
2021-10-12 22:30:42 -0500

    added SEs

--- src/ols.jl
+++ src/ols.jl
@@ -1,63 +1,60 @@
-abstract type Model end
-abstract type LinearModel <: Model  end
-abstract type DoesThomasEvenReadThis <: Model end
 
 mutable struct OLSModel <: LinearModel
     y::Vector
     X::Matrix
+    vcov::String
     β::Vector
+
     function OLSModel(y::Vector, X::Matrix)
+        length(y) == size(X, 1) || error("y and x have differing numbers
+        of observations.")
         new(y, X)
     end
+    function OLSModel(y::Vector, X::Matrix, vcov::String)
+        length(y) == size(X, 1) || error("y and x have differing numbers
+        of observations.")
+        new(y, X, vcov)
+    end
+
 end
 
+"""
+    fit!(model::OLSModel)
 
+Use the QR decomposition to estimate y = X β and return β.
+"""
 function fit!(model::OLSModel)
     y = model.y
     X = model.X
+    # QR decomposition for numerical stability
     β = X \ y 
     return β
 end
 
+"""
+    fit(model::OLSModel)
+As above but no longer in place returning β.
+"""
 function fit(model::OLSModel)
     model.β = fit!(model)
     return model
 end
 
-coef(model::OLSModel) = model.β  
-"""
-    fit(model::Model)
-Fit model.
-"""
-fit(model::Model) = error("Fit undefined for $(typeof(model))")
-
 
 
 """
-    dependentvariable(model::Model)
-Dependent variable of the model.
+    coef(model::OLSModel)
+For now just returns β coefs.
 """
-dependentvariable(model::Model) = error("dependentvariable undefined for $(typeof(model))")
+coef(model::OLSModel) = model.β 
 
+function summary(model::OLSModel)
+    nobs = length(model.y)
+    vcov = model.vcov
+    estimates = model.β
 
-"""
-    designmatrix(model::Model)
-Design matrix of the model.
-"""
-designmatrix(model::Model) = error("designmatrix undefined for $(typeof(model))")
+    printstyled("Observations: $(nobs)\n", color = :red)
+    printstyled("vcov: $(standarderrors)", color = :green)
+    printstyled("Estimates: $(estimates)", color = :blue)
 
-
-
-"""
-    coef(model::Model)
-Model coefficients.
-"""
-coef(model::Model) = error("coef undefined for $(typeof(model))")
-
-
-
-"""
-    vcov(model::Model)
-Model variance-covariance matrix.
-"""
-vcov(model::Model) = error("Variance-covariance matrix undefined for $(typeof(model))")
+end
